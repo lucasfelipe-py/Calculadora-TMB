2a3e47090907c19f09a603403a03be875482b84b
lucasfelipe-py <educacionalyt@gmail.com>
2022-07-02 11:48:20 -0300

    Update main.py
    Alteração entrada de dados usuário (taxa de atividade)

--- main.py
+++ main.py
@@ -44,22 +44,28 @@ app_linha = Label(frame_cima, text='', width=600, height=0, padx=0, relief='flat
 app_linha.place(x=0, y=62)
 
 #----------------------------------------------------------------------------------------------
-
-# Conteúdo frame_baixo
-    # Descrições TAXA DE ATIVIDADE
-texto(0, 'Entre com a sua taxa de atividade com base nos critérios estabelecidos abaixo:', 'Ivy 10 bold')
-texto(1, '1 - Sedentários (pouco ou nenhum exercício)')
-texto(2, '2 - Levemente ativo (exercício leve - 1 a 3 dias por semana)')
-texto(3, '3 - Moderadamente ativo (exercício moderado - 3 a 5 dias por semana)')
-texto(4, '4 - Altamente ativo (exercício pesado - 5 a 6 dias por semana)')
-texto(5, '5 - Extremamente ativo (exercício pesado diariamente e até 2 vezes por dia)')
-
+# Conteúdo frame_baixo    
+    
     # Entrada TAXA DE ATIVIDADE (Texto)
-texto(6, 'Digite o número correspondente à sua taxa de atividade:', 'Ivy 10 bold')
+texto(0, 'Escolha a opção correspondente à sua taxa de atividade:', 'Ivy 10 bold')
 
-    # Entrada TAXA DE ATIVIDADE (Entrada usuário)
-entrada_taxaAtividade = Entry(frame_baixo, width=10, relief='solid',font=('Ivy 10 bold'))
-entrada_taxaAtividade.grid(row=7, column=0, sticky=NSEW, pady=5, padx=35)
+    # Descrições TAXA DE ATIVIDADE
+entrada_taxas = StringVar()
+
+taxa_1 = Radiobutton(frame_baixo, text='Sedentário (pouco ou nenhum exercício)', font='Ivy 10', value='1', variable=entrada_taxas)
+taxa_1.grid(row=1, column=0, sticky=NSEW, pady=1, padx=35)
+
+taxa_2 = Radiobutton(frame_baixo, text='Levemente ativo (exercício leve - 1 a 3 dias por semana)', font='Ivy 10', value='2', variable=entrada_taxas)
+taxa_2.grid(row=2, column=0, sticky=NSEW, pady=1, padx=35)
+
+taxa_3 = Radiobutton(frame_baixo, text='Moderadamente ativo (exercício moderado - 3 a 5 dias por semana)', font='Ivy 10', value='3', variable=entrada_taxas)
+taxa_3.grid(row=3, column=0, sticky=NSEW, pady=1, padx=35)
+
+taxa_4 = Radiobutton(frame_baixo, text='Muito ativo (exercício pesado - 5 a 6 dias por semana)', font='Ivy 10', value='4', variable=entrada_taxas)
+taxa_4.grid(row=4, column=0, sticky=NSEW, pady=1, padx=35)
+
+taxa_5 = Radiobutton(frame_baixo, text='Extremamente ativo (exercício pesado diariamente e até 2 vezes por dia)', font='Ivy 10', value='5', variable=entrada_taxas)
+taxa_5.grid(row=5, column=0, sticky=NSEW, pady=1, padx=35)
 
 #----------------------------------------------------------------------------------------------
 
@@ -109,7 +115,7 @@ saida_resultado.grid(row=16, column=0, sticky=NSEW, pady=20, padx=0)
 # Função calcular
 def calcular():
     
-    taxa_entrada_usuario = int(entrada_taxaAtividade.get())
+    taxa_entrada_usuario = int(entrada_taxas.get())
     sexo_rec = entrada_sexo.get().lower()
     altura = int(entrada_altura.get())
     peso = int(entrada_peso.get())
@@ -117,13 +123,13 @@ def calcular():
 
     if taxa_entrada_usuario == 1:
             taxa_atividade = 1.2
-    elif taxa_entrada_usuario == 2:
+    if taxa_entrada_usuario == 2:
             taxa_atividade = 1.375
-    elif taxa_entrada_usuario == 3:
+    if taxa_entrada_usuario == 3:
             taxa_atividade = 1.55
-    elif taxa_entrada_usuario == 4:
+    if taxa_entrada_usuario == 4:
             taxa_atividade = 1.725
-    else:
+    if taxa_entrada_usuario == 5:
             taxa_atividade = 1.9
             
     if sexo_rec == 'feminino':
